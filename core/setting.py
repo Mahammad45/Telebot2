db6ca28d67065c4d4f439a5cfb8a11a4d7627059
Though0123 <mustafayev6111@gmail.com>
2024-08-16 16:00:34 +0500

    U

--- core/setting.py
+++ core/setting.py
@@ -1,4 +1,14 @@
 from configparser import ConfigParser
 
-config = ConfigParser
-config.read('config.ini')
+CONFIG = ConfigParser
+CONFIG.read('config.ini')
+
+TOKEN = CONFIG.get('telegram','TOKEN')
+
+
+
+PGHOST = CONFIG.get('database', 'PGHOST')
+PGDATABASE = CONFIG.get('database', 'PGDATABASE')
+PGUSER = CONFIG.get('database', 'PGUSER')
+PGPASSWORD = CONFIG.get('database', 'PGPASSWORD')
+PGPORT = CONFIG.get('database', 'PGPORT')
