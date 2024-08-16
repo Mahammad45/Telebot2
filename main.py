275c2346bdcae941289aef1fdbdecdace35b84a9
Though0123 <mustafayev6111@gmail.com>
2024-08-16 15:47:24 +0500

    U

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+import logging
+from random import randint, choice
+from datetime import datetime, timedelta
+
+
+import telebot
+from telebot import types
+
+from core.settings import TOKEN, PGCONNECTION
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
+bot = telebot.TeleBot(TOKEN)
+
+
+
+if __name__ == "__main__":
+    try:
+        
+        logger.info("Starting the application...")
+        bot.infinity_polling()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
